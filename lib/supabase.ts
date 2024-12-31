05b59370b69e08f4e63709a4847e10fb114bd49f
Sanjib Dahal <sanjibdahal04@gmail.com>
2025-01-01 00:15:38 +0545

    feat: implement AuthProvider for user authentication management

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,10 +2,10 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://graetfetdvajtneaoxnj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyYWV0ZmV0ZHZhanRuZWFveG5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5NDIxNjcsImV4cCI6MjA0OTUxODE2N30.679gndyuSAvXV9hpLMhNn5A9_rzashRofaijguU0Sl8';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANNON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
